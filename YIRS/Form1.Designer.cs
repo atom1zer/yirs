c364eb13cc9a2f08125065d8c332589e890e2a33
atom1zer <dlsamsonov201133@gmail.com>
2021-08-26 22:31:00 +0300

    Добавьте файлы проекта.

---
+++ YIRS/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace YIRS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(328, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Случайное заполнение массива";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сортировка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество элементов массива";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Искомый элемент";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(569, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(209, 212);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Время сортировки в мс";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Время поиска в тиках";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ YIRS/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YIRS
+{
+    public partial class Form1 : Form
+    {
+        public int size;
+        public int[] array;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        //метод для рекурсивного бинарного поиска
+        int BinarySearch(int[] array, int searchedValue, int first, int last)
+        {
+            var stopWatch = Stopwatch.StartNew(); // начало отсчета таймера
+            //средний индекс подмассива
+            var middle = first + (last - first) / 2;
+            //значение в средине подмассива
+            var middleValue = array[middle];
+           // MessageBox.Show("M" + middle + "  V" + middleValue);
+
+
+            //границы сошлись
+            if (first > last)
+            {
+                //элемент не найден
+                // MessageBox.Show("first" + first + "  last" + last);
+                stopWatch.Stop();
+                label4.Text = stopWatch.ElapsedMilliseconds.ToString(); // в милисекундах 
+                return -1;
+               
+            }
+
+           
+            if (middleValue == searchedValue)
+            {
+                stopWatch.Stop();
+                label4.Text = stopWatch.ElapsedTicks.ToString(); // в тиках (1 тик = 100нс)
+                return middle;
+            }
+            else
+            {
+                if (middleValue > searchedValue)
+                {
+                    //рекурсивный вызов поиска для левого подмассива
+                  //  MessageBox.Show(" Больше" );
+                    return BinarySearch(array, searchedValue, first, middle - 1);
+                    
+
+                }
+                else
+                {
+                    //рекурсивный вызов поиска для правого подмассива
+                   // MessageBox.Show("Меньше");
+                    return BinarySearch(array, searchedValue, middle + 1, last);
+                    
+                }
+            }
+
+             
+        }
+         int[] BubbleSort(int[] mas)
+        {
+            var stopWatch = Stopwatch.StartNew(); // начало отсчета таймера
+                                                 
+
+            int temp;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[i] > mas[j])
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+                    }
+                }
+
+            }
+
+            stopWatch.Stop();
+
+            label3.Text = stopWatch.ElapsedMilliseconds.ToString(); // в милисекундах  
+
+            return mas;
+
+        }
+      
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            size = Convert.ToInt32(textBox1.Text);
+            array = new int[size];
+            int temp;
+            Random rand = new Random();
+            for (int y = 0; y < size; y++)
+            {
+                temp = rand.Next(1, size+1);
+                if (!array.Contains(temp))
+                {
+                    array[y] = temp;
+                }
+                else
+                {
+                    y-=1;
+                }
+            }
+           // listBox1.Items.AddRange(Convert.ToString(array));
+            listBox1.Items.Clear();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                listBox1.Items.Add(array[i].ToString());
+            }
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            BubbleSort(array);
+            
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                listBox2.Items.Add(array[i].ToString());
+            }
+
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+         
+                var k = Convert.ToInt32(textBox2.Text);
+                
+                var searchResult = BinarySearch(array, k, 0, array.Length - 1);
+            
+            if (searchResult < 0)
+                {
+                MessageBox.Show(" "+ searchResult);
+                MessageBox.Show("Элемент со значением " + k + " не найден");
+                }
+                else
+                {
+                    MessageBox.Show("Элемент найден. Индекс элемента со значением " + k + " равен " + searchResult);
+                    searchResult = 0;
+                }
+            
+        }
+    }
+}
